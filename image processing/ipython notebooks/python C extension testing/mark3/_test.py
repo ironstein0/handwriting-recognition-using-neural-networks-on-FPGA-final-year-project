e0935a442e8cabe070ee8f5f19bd63d165e8ffcf
Riyansh Karani <riyansh.karani.011235@gmail.com>
2015-10-28 13:06:56 +0530

    adding final testing code for C binding in python

--- image processing/ipython notebooks/python C extension testing/mark3/_test.py
+++ image processing/ipython notebooks/python C extension testing/mark3/_test.py
@@ -3,8 +3,13 @@ import numpy
 import ctypes
 
 _test_ = numpy.ctypeslib.load_library('test','.')
-_test_.for_loop_test.argtypes = [ctypes.c_int]
-_test_.for_loop_test.restype = ctypes.c_void_p
+_test_.for_loop_test_without_register_values.argtypes = [ctypes.c_int]
+_test_.for_loop_test_without_register_values.restype = ctypes.c_void_p
+_test_.for_loop_test_with_register_values.argtypes = [ctypes.c_int]
+_test_.for_loop_test_with_register_values.restype = ctypes.c_void_p
 
-def C_for_test(n) : 
-    _test_.for_loop_test(n)
+def C_for_test_without_register_values(n) : 
+    _test_.for_loop_test_without_register_values(n)
+    
+def C_for_test_with_register_values(n) : 
+    _test_.for_loop_test_with_register_values(n)
