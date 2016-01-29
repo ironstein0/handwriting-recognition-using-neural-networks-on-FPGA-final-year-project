552f58169ba0ec6b0ea3ac7cc2cdce19dd7aed18
Riyansh Karani <riyansh.karani.011235@gmail.com>
2016-01-29 15:18:25 +0530

    updated ran_codes

--- neural_networks/rana_codes/src/Analysis.py
+++ neural_networks/rana_codes/src/Analysis.py
@@ -1,3 +1,33 @@
+#------------------------ CONSTANTS ---------------------------
+hidden_layer_neurons = 30
+eta = 3.0
+epochs = 10
+mini_batch_size = 10
+#--------------------------------------------------------------
+
+#----------------------- INITIALIZATION ------------------------
+import sys
+SYSTEM_PATH_SEPERATOR = ''
+if(sys.platform.startswith('darwin')) : 
+    SYSTEM_PATH_SEPERATOR = '/'
+elif sys.platform.startswith('win') : 
+    SYSTEM_PATH_SEPERATOR = '\\'
+else : 
+    raise ValueError
+
+def get_saving_directory() : 
+    saving_directory = __file__.split(SYSTEM_PATH_SEPERATOR)[:-2]
+    saving_directory.append('test_results')
+    temp_saving_directory = ''
+    for element in saving_directory : 
+        temp_saving_directory += element + SYSTEM_PATH_SEPERATOR
+    saving_directory = temp_saving_directory
+    
+    return saving_directory
+
+SAVING_DIRECTORY = get_saving_directory()
+print(SAVING_DIRECTORY)
+
 import mnist_loader
 import matplotlib.pyplot as ppt
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
@@ -7,12 +37,13 @@ import pylab
 net = network.Network([784, 30, 10])
 
 def looping(b,c,x):
+    global SAVING_DIRECTORY
     
     for j in range (0,8):
     	for k in range(0,10):
             if (c >= x+1):
                 c = x
-            location = ["C:\Users\Akash\Pictures\Neural_Net_graphs2\\"]
+            location = [SAVING_DIRECTORY]
             temp = []
             epoch_efficiency_list = net.SGD(training_data,a,b,c,test_data = test_data)
             t = []
--- neural_networks/rana_codes/src/mnist_loader.py
+++ neural_networks/rana_codes/src/mnist_loader.py
@@ -7,7 +7,7 @@ import gzip
 import numpy as np
 
 def load_data():
-    f = gzip.open('mnist.pkl.gz', 'rb')
+    f = gzip.open('../data/mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = cPickle.load(f)
     f.close()
     return (training_data, validation_data, test_data)
