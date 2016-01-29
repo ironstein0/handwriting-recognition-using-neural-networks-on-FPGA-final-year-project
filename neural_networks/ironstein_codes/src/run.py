4449b416869b00e6717a12c9de1a717dc43217e3
Riyansh Karani <riyansh.karani.011235@gmail.com>
2016-01-29 23:37:36 +0530

    commit just before starting testing (for alpha)

--- neural_networks/ironstein_codes/src/run.py
+++ neural_networks/ironstein_codes/src/run.py
@@ -1,7 +1,7 @@
 #------------------------ CONSTANTS ---------------------------
 hidden_layer_neurons = 10
 eta = 1.0
-epochs = 5
+epochs = 15
 mini_batch_size = 15
 #--------------------------------------------------------------
 
@@ -17,7 +17,7 @@ else :
 
 def get_saving_directory() : 
 	saving_directory = __file__.split(SYSTEM_PATH_SEPERATOR)[:-2]
-	saving_directory.append('results')
+	saving_directory.append('test_results')
 	temp_saving_directory = ''
 	for element in saving_directory : 
 		temp_saving_directory += element + SYSTEM_PATH_SEPERATOR
@@ -32,13 +32,18 @@ import network
 training_data,validation_data,test_data = mnist_loader.load_data_wrapper()
 #----------------------------------------------------------------
 import matplotlib.pyplot as plt
+import pylab
 
 def loop() : 
 	global GRAPHS_SAVING_DIRECTORY
+	global mini_batch_size
+	global epochs
+	net = network.Network([784,hidden_layer_neurons,10])
+	for eta in [i*0.1 for i in range(50)][1:] :
+		performance_list = net.SGD(training_data, epochs, mini_batch_size, eta, test_data=test_data)
+		epoch_list = [i for i in range(epochs)]
+		plt.plot(epoch_list,performance_list,'b-')
+		pylab.ylim([0,10000])
+		plt.savefig(GRAPHS_SAVING_DIRECTORY + 'eta_' + str(eta) + '.png')
 
-
-net = network.Network([784,hidden_layer_neurons,10])
-performance_list = net.SGD(training_data, epochs, mini_batch_size, eta, test_data=test_data)
-epoch_list = [i for i in range(epochs)]
-plt.plot(epoch_list,performance_list,'b-')
-plt.show()
+loop()
