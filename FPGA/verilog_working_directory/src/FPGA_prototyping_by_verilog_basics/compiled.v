71069c30ba57c3f78d100dd913c048ac3c132fe1
Riyansh Karani <riyansh.karani.011235@gmail.com>
2016-02-18 10:42:44 +0530

    completed working bliinky

--- FPGA/verilog_working_directory/src/FPGA_prototyping_by_verilog_basics/compiled.v
+++ FPGA/verilog_working_directory/src/FPGA_prototyping_by_verilog_basics/compiled.v
@@ -7,67 +7,118 @@
 :vpi_module "vhdl_textio";
 :vpi_module "v2005_math";
 :vpi_module "va_math";
-S_0x7fe10a403520 .scope module, "stimulus" "stimulus" 2 20;
+S_0x7ff101c07590 .scope module, "stimulus" "stimulus" 2 48;
  .timescale 0 0;
-v0x7fe10a502af0_0 .var "DUMMY", 0 0;
-v0x7fe10a502b80_0 .net "EQ", 0 0, L_0x7fe10a502d90;  1 drivers
-v0x7fe10a502c10_0 .var "I0", 0 0;
-v0x7fe10a502ce0_0 .var "I1", 0 0;
-S_0x7fe10a4036b0 .scope module, "E" "eq1" 2 25, 2 1 0, S_0x7fe10a403520;
+v0x7ff101c1e910_0 .net "a2", 0 0, v0x7ff101c1e300_0;  1 drivers
+v0x7ff101c1e9e0_0 .net "b2", 0 0, v0x7ff101c1e3a0_0;  1 drivers
+v0x7ff101c1eab0_0 .net "b3", 0 0, v0x7ff101c1e440_0;  1 drivers
+v0x7ff101c1eb80_0 .var "clk", 0 0;
+v0x7ff101c1ec50_0 .var "dummy", 0 0;
+v0x7ff101c1ed20_0 .net "led", 0 0, v0x7ff101c1dfe0_0;  1 drivers
+S_0x7ff101c07720 .scope module, "b" "blinky" 2 51, 2 1 0, S_0x7ff101c07590;
  .timescale 0 0;
-    .port_info 0 /INPUT 1 "i0"
-    .port_info 1 /INPUT 1 "i1"
-    .port_info 2 /OUTPUT 1 "eq"
-L_0x7fe10a502d90 .functor OR 1, L_0x7fe10a502fe0, L_0x7fe10a503130, C4<0>, C4<0>;
-L_0x7fe10a502e80 .functor NOT 1, v0x7fe10a502c10_0, C4<0>, C4<0>, C4<0>;
-L_0x7fe10a502f30 .functor NOT 1, v0x7fe10a502ce0_0, C4<0>, C4<0>, C4<0>;
-L_0x7fe10a502fe0 .functor AND 1, L_0x7fe10a502e80, L_0x7fe10a502f30, C4<1>, C4<1>;
-L_0x7fe10a503130 .functor AND 1, v0x7fe10a502c10_0, v0x7fe10a502ce0_0, C4<1>, C4<1>;
-v0x7fe10a406100_0 .net *"_s2", 0 0, L_0x7fe10a502e80;  1 drivers
-v0x7fe10a502750_0 .net *"_s4", 0 0, L_0x7fe10a502f30;  1 drivers
-v0x7fe10a5027e0_0 .net "eq", 0 0, L_0x7fe10a502d90;  alias, 1 drivers
-v0x7fe10a502870_0 .net "i0", 0 0, v0x7fe10a502c10_0;  1 drivers
-v0x7fe10a502900_0 .net "i1", 0 0, v0x7fe10a502ce0_0;  1 drivers
-v0x7fe10a5029d0_0 .net "p0", 0 0, L_0x7fe10a502fe0;  1 drivers
-v0x7fe10a502a60_0 .net "p1", 0 0, L_0x7fe10a503130;  1 drivers
-    .scope S_0x7fe10a403520;
+    .port_info 0 /INPUT 1 "CLK"
+    .port_info 1 /OUTPUT 1 "LED"
+    .port_info 2 /OUTPUT 1 "A2"
+    .port_info 3 /OUTPUT 1 "B2"
+    .port_info 4 /OUTPUT 1 "B3"
+v0x7ff101c1e540_0 .net "A2", 0 0, v0x7ff101c1e300_0;  alias, 1 drivers
+v0x7ff101c1e5e0_0 .net "B2", 0 0, v0x7ff101c1e3a0_0;  alias, 1 drivers
+v0x7ff101c1e690_0 .net "B3", 0 0, v0x7ff101c1e440_0;  alias, 1 drivers
+v0x7ff101c1e760_0 .net "CLK", 0 0, v0x7ff101c1eb80_0;  1 drivers
+v0x7ff101c1e810_0 .net "LED", 0 0, v0x7ff101c1dfe0_0;  alias, 1 drivers
+S_0x7ff101c04260 .scope module, "b" "blink" 2 12, 2 15 0, S_0x7ff101c07720;
+ .timescale 0 0;
+    .port_info 0 /INPUT 1 "clk"
+    .port_info 1 /OUTPUT 1 "led"
+P_0x7ff101c06390 .param/l "seconds" 0 2 18, C4<10>;
+v0x7ff101c078b0_0 .net "clk", 0 0, v0x7ff101c1eb80_0;  alias, 1 drivers
+v0x7ff101c1de70_0 .var "count", 32 0;
+v0x7ff101c1df20_0 .var "expected_count", 32 0;
+v0x7ff101c1dfe0_0 .var "led", 0 0;
+E_0x7ff101c089d0 .event posedge, v0x7ff101c078b0_0;
+S_0x7ff101c1e0b0 .scope module, "i" "initialize_all" 2 6, 2 36 0, S_0x7ff101c07720;
+ .timescale 0 0;
+    .port_info 0 /OUTPUT 1 "a2"
+    .port_info 1 /OUTPUT 1 "b2"
+    .port_info 2 /OUTPUT 1 "b3"
+v0x7ff101c1e300_0 .var "a2", 0 0;
+v0x7ff101c1e3a0_0 .var "b2", 0 0;
+v0x7ff101c1e440_0 .var "b3", 0 0;
+    .scope S_0x7ff101c1e0b0;
 T_0 ;
-    %vpi_call 2 29 "$dumpfile", "simulation.vcd" {0 0 0};
-    %vpi_call 2 30 "$dumpvars", v0x7fe10a502af0_0, v0x7fe10a502c10_0, v0x7fe10a502ce0_0, v0x7fe10a502b80_0 {0 0 0};
+    %pushi/vec4 1, 0, 1;
+    %store/vec4 v0x7ff101c1e300_0, 0, 1;
+    %pushi/vec4 1, 0, 1;
+    %store/vec4 v0x7ff101c1e3a0_0, 0, 1;
+    %pushi/vec4 1, 0, 1;
+    %store/vec4 v0x7ff101c1e440_0, 0, 1;
     %end;
     .thread T_0;
-    .scope S_0x7fe10a403520;
+    .scope S_0x7ff101c04260;
 T_1 ;
-    %pushi/vec4 0, 0, 1;
-    %store/vec4 v0x7fe10a502c10_0, 0, 1;
-    %pushi/vec4 0, 0, 1;
-    %store/vec4 v0x7fe10a502ce0_0, 0, 1;
+    %pushi/vec4 0, 0, 33;
+    %store/vec4 v0x7ff101c1de70_0, 0, 33;
     %end;
     .thread T_1;
-    .scope S_0x7fe10a403520;
+    .scope S_0x7ff101c04260;
 T_2 ;
-    %delay 1, 0;
-    %load/vec4 v0x7fe10a502c10_0;
-    %inv;
-    %store/vec4 v0x7fe10a502c10_0, 0, 1;
-    %jmp T_2;
-    .thread T_2;
-    .scope S_0x7fe10a403520;
-T_3 ;
-    %delay 2, 0;
-    %load/vec4 v0x7fe10a502ce0_0;
-    %inv;
-    %store/vec4 v0x7fe10a502ce0_0, 0, 1;
-    %jmp T_3;
-    .thread T_3;
-    .scope S_0x7fe10a403520;
-T_4 ;
-    %delay 4, 0;
-    %vpi_call 2 46 "$finish" {0 0 0};
+    %pushi/vec4 20, 0, 33;
+    %store/vec4 v0x7ff101c1df20_0, 0, 33;
     %end;
+    .thread T_2;
+    .scope S_0x7ff101c04260;
+T_3 ;
+    %pushi/vec4 1, 0, 1;
+    %store/vec4 v0x7ff101c1dfe0_0, 0, 1;
+    %end;
+    .thread T_3;
+    .scope S_0x7ff101c04260;
+T_4 ;
+    %wait E_0x7ff101c089d0;
+    %load/vec4 v0x7ff101c1de70_0;
+    %addi 1, 0, 33;
+    %store/vec4 v0x7ff101c1de70_0, 0, 33;
+    %load/vec4 v0x7ff101c1de70_0;
+    %load/vec4 v0x7ff101c1df20_0;
+    %cmp/e;
+    %jmp/0xz  T_4.0, 4;
+    %load/vec4 v0x7ff101c1dfe0_0;
+    %inv;
+    %store/vec4 v0x7ff101c1dfe0_0, 0, 1;
+    %pushi/vec4 0, 0, 33;
+    %store/vec4 v0x7ff101c1de70_0, 0, 33;
+T_4.0 ;
+    %jmp T_4;
     .thread T_4;
+    .scope S_0x7ff101c07590;
+T_5 ;
+    %vpi_call 2 54 "$dumpfile", "simulation.vcd" {0 0 0};
+    %vpi_call 2 55 "$dumpvars", v0x7ff101c1ec50_0, v0x7ff101c1eb80_0, v0x7ff101c1ed20_0 {0 0 0};
+    %end;
+    .thread T_5;
+    .scope S_0x7ff101c07590;
+T_6 ;
+    %pushi/vec4 0, 0, 1;
+    %store/vec4 v0x7ff101c1eb80_0, 0, 1;
+    %end;
+    .thread T_6;
+    .scope S_0x7ff101c07590;
+T_7 ;
+    %delay 1, 0;
+    %load/vec4 v0x7ff101c1eb80_0;
+    %inv;
+    %store/vec4 v0x7ff101c1eb80_0, 0, 1;
+    %jmp T_7;
+    .thread T_7;
+    .scope S_0x7ff101c07590;
+T_8 ;
+    %delay 1000, 0;
+    %vpi_call 2 65 "$finish" {0 0 0};
+    %end;
+    .thread T_8;
 # The file index is used to find the file name in the following table.
 :file_names 3;
     "N/A";
     "<interactive>";
-    "verilog_general_description.v";
+    "02_blinky.v";
