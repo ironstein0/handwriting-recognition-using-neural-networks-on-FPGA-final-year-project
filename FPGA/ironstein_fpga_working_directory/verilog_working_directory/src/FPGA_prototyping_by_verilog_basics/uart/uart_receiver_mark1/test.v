11c7777993260eaea7d320b61d139f5f121f4943
Riyansh Karani <riyansh.karani.011235@gmail.com>
2016-02-22 04:24:35 +0530

    partially workig uart receiver core

---
+++ FPGA/ironstein_fpga_working_directory/verilog_working_directory/src/FPGA_prototyping_by_verilog_basics/uart/uart_receiver_mark1/test.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    05:15:37 02/21/2016 
+// Design Name: 
+// Module Name:    test 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module test(
+    );
+
+
+endmodule
