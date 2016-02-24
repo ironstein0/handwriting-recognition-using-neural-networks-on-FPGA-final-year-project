cdb836e18af13df36196094ef4940eecff38afba
Riyansh Karani <riyansh.karani.011235@gmail.com>
2016-02-24 12:12:47 +0530

    working binary_to_12bit_bcd_converter simulation, but incorrect synthesis

---
+++ FPGA/ironstein_fpga_working_directory/xilinx_ise_project_files/binary_to_12bit_bcd_converter_mark2/binary_to_bcd_converter_mark2_verilog_modules/stimulus.v
@@ -0,0 +1,6 @@
+`include "seven_segment_display_controller.v"
+`include "binary_to_12bit_bcd_converter.v"
+
+module stimulus(
+	input wire CLK,
+	
---
+++ FPGA/ironstein_fpga_working_directory/xilinx_ise_project_files/binary_to_12bit_bcd_converter_mark2/stimulus.v
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+`include "seven_segment_display_controller.v"
+`include "binary_to_12bit_bcd_converter.v"
+module stimulus(
+    );
+
+
+endmodule
