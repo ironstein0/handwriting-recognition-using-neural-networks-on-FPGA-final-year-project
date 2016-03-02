75b89ba801aa058120dbbc9063300e3a2033b48a
Riyansh Karani <riyansh.karani.011235@gmail.com>
2016-03-02 15:10:55 +0530

    Working simulation and synthesis debouncing switch

--- FPGA/ironstein_fpga_working_directory/verilog_working_directory/src/FPGA_prototyping_by_verilog_basics/debouncing/tick_generator.v
+++ FPGA/ironstein_fpga_working_directory/verilog_working_directory/src/FPGA_prototyping_by_verilog_basics/debouncing/tick_generator.v
@@ -6,7 +6,7 @@ module tick_generator #(
 	output reg tick);
 
 	reg [32:0] count;
-	reg [32:0] count_threshold = tick_time * frequency / 2;
+	localparam [32:0] count_threshold = tick_time * frequency / 2;
 	
 	initial begin
 		tick = 1'b0;
@@ -17,31 +17,34 @@ module tick_generator #(
 		count = count + 1;
 		if(count == count_threshold) begin
 			count = 0;
-			tick = ~ tick;
+			tick = 1;
+		end
+		else begin
+			tick = 0;
 		end
 	end
 
 endmodule
 
 // module stimulus();
-
+//
 // 	reg CLK;
 // 	wire TICK;
-
+//
 // 	tick_generator #(.tick_time(32'd5),.frequency(32'd1)) t1(.clk(CLK),.tick(TICK));
-
+//
 // 	initial begin
 // 		$dumpfile("simulation.vcd");
 // 		$dumpvars(1,CLK,TICK);
 // 	end
-
+//
 // 	initial	
 // 		CLK = 1'b0;
-
+//
 // 	always
 // 		#1 CLK = ~ CLK;
-
+//
 // 	initial
 // 		#100 $finish;
-
+//
 // endmodule
