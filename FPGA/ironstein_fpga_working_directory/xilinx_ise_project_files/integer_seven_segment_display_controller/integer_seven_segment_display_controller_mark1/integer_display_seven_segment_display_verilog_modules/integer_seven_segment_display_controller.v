b05516a9015bf52f772c32c1699ed650aa0b4281
Riyansh Karani <riyansh.karani.011235@gmail.com>
2016-03-04 19:20:43 +0530

    Working simulation and synthesis integer seven segment display

---
+++ FPGA/ironstein_fpga_working_directory/xilinx_ise_project_files/integer_seven_segment_display_controller/integer_seven_segment_display_controller_mark1/integer_display_seven_segment_display_verilog_modules/integer_seven_segment_display_controller.v
@@ -0,0 +1,171 @@
+`include "binary_to_12bit_bcd_converter.v"
+`include "seven_segment_display_controller.v"
+`include "pulse_generator.v"
+
+module integer_seven_segment_display_controller(
+	input wire clk,
+	output wire [7:0] display_bits,
+	output wire led1_control_signal,
+	output wire led2_control_signal,
+	output wire led3_control_signal,
+	output wire led_change_tick,
+	input wire [7:0] display_integer,
+	output wire state_debug,
+	output wire [11:0] CONVERTED_DATA
+	);
+	
+	// ------------ module instantiations ----------
+//	wire [11:0] CONVERTED_DATA;
+	wire BCD_START_CONVERSION;
+	wire BCD_CONVERSION_COMPLETE;
+	
+	binary_to_12bit_bcd_converter #(.BINARY_DATA_SIZE(4'd8))
+		c(
+			.clk(clk),
+			.binary_data(display_integer),
+			.bcd_data(CONVERTED_DATA),
+			.start_conversion(BCD_START_CONVERSION),
+			.conversion_complete(BCD_CONVERSION_COMPLETE)
+		);
+
+	seven_segment_display_controller ssdc1(
+		.clk(clk),
+		.display_bits(display_bits),
+		.led1_control_signal(led1_control_signal),
+		.led2_control_signal(led2_control_signal),
+		.led3_control_signal(led3_control_signal),
+		.led_change_tick(led_change_tick),
+		.led1_display_value(CONVERTED_DATA[11:8]),
+		.led2_display_value(CONVERTED_DATA[7:4]),
+		.led3_display_value(CONVERTED_DATA[3:0])
+	);
+
+	reg START_CONVERSION = 1'b0;
+	pulse_generator #(.PULSE_WIDTH(32'b1))
+		generate_conversion_start_pulse(
+			.clk(clk),
+			.generate_pulse(START_CONVERSION),
+			.pulse(BCD_START_CONVERSION)
+		);
+
+	// ------------ state declarations --------------
+	localparam 
+		idle = 1'b0,
+		converting = 1'b1;
+
+	// signal declarations 
+	reg state_reg = idle, state_next = idle;
+	assign state_debug = state_reg;
+
+	// state updation 
+	always @(posedge(clk)) begin
+		state_reg <= state_next;
+	end
+
+	always @(display_integer) begin
+		if(state_reg == idle) begin
+			START_CONVERSION = ~ START_CONVERSION;
+		end
+	end
+
+	// next state logic
+	always @(posedge(BCD_START_CONVERSION),posedge(BCD_CONVERSION_COMPLETE)) begin
+		if(BCD_START_CONVERSION) begin
+			state_next = converting;
+		end
+
+		if(BCD_CONVERSION_COMPLETE) begin
+			state_next = idle;
+		end
+	end
+ 
+endmodule
+//
+//module stimulus(
+//	input wire CLK,
+//	output wire [7:0] DISPLAY_BITS,
+//	output wire LED1_CONTROL_SIGNAL, 
+//	output wire LED2_CONTROL_SIGNAL,
+//	output wire LED3_CONTROL_SIGNAL,
+//	output wire NUMBER_CHANGE_TICK
+//	);
+//	
+//	reg [7:0] DISPLAY_INTEGER = 8'b00000001;
+//	wire STATE_DEBUG;
+//	wire CONVERTED_DATA;
+//	
+//	integer_seven_segment_display_controller s(
+//		.clk(CLK),
+//		.display_bits(DISPLAY_BITS),
+//		.led1_control_signal(LED1_CONTROL_SIGNAL),
+//		.led2_control_signal(LED2_CONTROL_SIGNAL),
+//		.led3_control_signal(LED3_CONTROL_SIGNAL),
+//		.led_change_tick(LED_CHANGE_TICK),
+//		.display_integer(DISPLAY_INTEGER),
+//		.state_debug(STATE_DEBUG),
+//		.CONVERTED_DATA(CONVERTED_DATA)
+//	);
+//	
+//	always @(posedge(NUMBER_CHANGE_TICK)) begin
+//		DISPLAY_INTEGER = DISPLAY_INTEGER + 1;
+//	end
+//	
+////	wire NUMBER_CHANGE_TICK;
+//	tick_generator #(.tick_time(32'd100),.frequency(32'd100000))
+//		tg(
+//			.clk(CLK),
+//			.tick(NUMBER_CHANGE_TICK)
+//		);
+////		
+////	always begin
+////		#100000000 DISPLAY_INTEGER = DISPLAY_INTEGER + 1;
+////	end
+//
+// endmodule
+//
+//module stimulus();
+//
+//	reg clk = 1'b0;
+//	reg [7:0] display_integer = 8'b00000000;
+//	wire [7:0] display_bits;
+//	wire led1_control_signal, led2_control_signal, led3_control_signal;
+//	wire led_change_tick;
+//
+//	integer_seven_segment_display_controller s(
+//		.clk(clk),
+//		.display_bits(display_bits),
+//		.led1_control_signal(led1_control_signal),
+//		.led2_control_signal(led2_control_signal),
+//		.led3_control_signal(led3_control_signal),
+//		.led_change_tick(led_change_tick),
+//		.display_integer(display_integer)
+//	);
+//
+//	initial begin
+//		$dumpfile("simulation.vcd");
+//		$dumpvars(0,
+//			clk,
+//			display_bits,
+//			led1_control_signal,
+//			led2_control_signal,
+//			led3_control_signal,
+//			led_change_tick,
+//			display_integer
+//		);
+//	end
+//
+//	always begin
+//		#1 clk = ~ clk;
+//	end
+//
+//	initial begin
+//		#10 display_integer = 8'b11111111;
+//		#200 display_integer = 8'b00001111;
+//	end
+//
+//	initial begin
+//		#1000 $finish;
+//	end
+//
+//endmodule
+
---
+++ FPGA/ironstein_fpga_working_directory/xilinx_ise_project_files/integer_seven_segment_display_controller/integer_seven_segment_display_controller_mark2/integer_seven_segment_display_core_mark2/binary_to_12bit_bcd_converter.v
@@ -0,0 +1,170 @@
+module binary_to_12bit_bcd_converter #(
+		parameter BINARY_DATA_SIZE = 4'd8
+	)(
+		input wire clk,
+		input wire [BINARY_DATA_SIZE-1:0] binary_data,
+		input wire start_conversion,
+		input wire reset,
+		output reg [11:0] bcd_data
+//		output reg conversion_complete,
+//		output wire [BINARY_DATA_SIZE + 11:0] shift_reg_debug,
+//		output wire [BINARY_DATA_SIZE + 11:0] shift_reg_next_debug,
+//		output reg debug,
+//		output wire state_debug
+	);
+
+	// state declarations
+	localparam 
+		idle = 1'b0,
+		converting = 1'b1;
+
+	// signal declarations
+	reg [BINARY_DATA_SIZE + 11:0] shift_reg, shift_reg_next;
+	reg state_reg, state_next; 
+	reg [3:0] count, count_next;
+	reg [11:0] bcd_data_next;
+	reg conversion_complete_next;
+//	assign shift_reg_debug = shift_reg;
+//	assign shift_reg_next_debug = shift_reg_next;
+//	assign state_debug = state_reg;
+	reg conversion_complete; //
+//	
+//	initial begin
+//		debug = 1'b0;
+//	end
+
+	// state updation logic
+	always @(posedge(clk),posedge(reset)) begin
+		state_reg <= state_next;
+		count <= count_next;
+		shift_reg <= shift_reg_next;
+		bcd_data <= bcd_data_next;
+		conversion_complete <= conversion_complete_next;
+	end
+
+	// next state logic
+	always @* begin
+		
+		if(reset) begin 
+			state_next <= idle;
+			count_next <= 4'b0;
+			bcd_data_next <= 12'b0;
+			shift_reg_next <= 0;
+			conversion_complete_next <= 1'b0; 
+		end else begin
+			case (state_reg)
+				idle :
+					if(start_conversion) begin
+						shift_reg_next = {12'b0,binary_data};
+						count_next = 0;
+						state_next = converting;
+					end else begin
+						conversion_complete_next = 1'b0;
+					end
+
+				converting :
+					if(count == BINARY_DATA_SIZE) begin
+						conversion_complete_next = 1'b1;
+						bcd_data_next = shift_reg[BINARY_DATA_SIZE+11:BINARY_DATA_SIZE];
+						state_next = idle;
+					end else begin
+						// updating d2
+						if(shift_reg == shift_reg_next) begin
+							if(shift_reg[19:16] > 4'd4) begin
+								shift_reg_next[19:16] = shift_reg[19:16] + 4'd3;
+							end else begin
+								shift_reg_next[19:16] = shift_reg[19:16];
+							end
+
+							// updating d1
+							if(shift_reg[15:12] > 4'd4) begin
+								shift_reg_next[15:12] = shift_reg[15:12] + 4'd3;
+							end else begin
+								shift_reg_next[15:12] = shift_reg[15:12];
+							end
+
+							// updating d0
+							if(shift_reg[11:8] > 4'd4) begin
+								shift_reg_next[11:8] = shift_reg[11:8] + 4'd3;
+							end else begin
+								shift_reg_next[11:8] = shift_reg[11:8];
+							end
+
+							shift_reg_next[7:0] = shift_reg_next[7:0];
+
+							// left shifting 
+							shift_reg_next = {shift_reg_next[BINARY_DATA_SIZE+10:0],1'b0};
+//							debug = ~ debug;
+							count_next = count + 1;
+						end
+					end
+			endcase
+		end
+	end
+endmodule
+
+// module stimulus();
+// 	reg CLK = 1'b0;
+// 	reg [7:0] BINARY_DATA = 8'b11110000;
+// 	reg START_CONVERSION = 1'b0;
+// 	wire CONVERSION_COMPLETE;
+// 	wire [11:0] BCD_DATA;
+// 	reg RESET;
+// 	wire [19:0] SHIFT_REG_DEBUG, SHIFT_REG_NEXT_DEBUG;
+// 	wire DEBUG, STATE_DEBUG;
+
+// 	binary_to_12bit_bcd_converter #(.BINARY_DATA_SIZE(4'd8))
+// 		converter(
+// 			.clk(CLK),
+// 			.binary_data(BINARY_DATA),
+// 			.bcd_data(BCD_DATA),
+// 			.start_conversion(START_CONVERSION),
+// 			.conversion_complete(CONVERSION_COMPLETE),
+// 			.reset(RESET),
+// 			.shift_reg_debug(SHIFT_REG_DEBUG),
+// 			.shift_reg_next_debug(SHIFT_REG_NEXT_DEBUG),
+// 			.debug(DEBUG),
+// 			.state_debug(STATE_DEBUG)
+// 		);
+
+// 	wire [7:0] DISPLAY_BITS;
+
+// 	initial begin
+// 		$dumpfile("simulation.vcd");
+// 		$dumpvars(0,
+// 			CLK,
+// 			BINARY_DATA,
+// 			START_CONVERSION,
+// 			CONVERSION_COMPLETE,
+// 			BCD_DATA,
+// 			DISPLAY_BITS,
+// 			RESET,
+// 			SHIFT_REG_DEBUG,
+// 			SHIFT_REG_NEXT_DEBUG,
+// 			DEBUG,
+// 			STATE_DEBUG
+// 		);
+// 	end
+
+// 	always begin
+// 		#1 CLK = ~ CLK;
+// 	end
+
+// 	initial begin
+// 		#10 RESET = 1'b1;
+// 		#1 RESET = 1'b0;
+// 		#10 START_CONVERSION = 1'b1;
+// 		#1 START_CONVERSION = 1'b0;
+// 		#10 BINARY_DATA = 8'b11111111;
+// 		#200 START_CONVERSION = 1'b1;
+// 		#1 START_CONVERSION = 1'b0;
+// 		#10 BINARY_DATA = 8'b10000001;
+// 		#200 START_CONVERSION = 1'b1;
+// 		#1 START_CONVERSION = 1'b0;
+		
+// 	end
+
+// 	initial begin
+// 		#1000 $finish;
+// 	end
+// endmodule
